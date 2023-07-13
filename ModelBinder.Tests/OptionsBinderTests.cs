eccf4f489e02a5af39b188cfc931cde541940ff1
michael berezin <mbearz@gmail.com>
2023-07-13 23:34:38 +0300

    refactor

--- ModelBinder.Tests/OptionsBinderTests.cs
+++ ModelBinder.Tests/OptionsBinderTests.cs
@@ -1,11 +1,9 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Primitives;
 using ModelBinder.Binders;
 using ModelBinder.Helper;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Primitives;
 using Moq;
-using Newtonsoft.Json.Serialization;
 
 namespace ModelBinder.Tests
 {
@@ -42,7 +40,6 @@ namespace ModelBinder.Tests
             Assert.AreEqual(bindingContext.Result.Model, expectedBool);
         }
 
-
         [TestMethod]
         public async Task OptionsBinder_NotValidValue()
         {
@@ -55,6 +52,26 @@ namespace ModelBinder.Tests
 
             context.Setup(c => c.HttpContext.Request.Query)
                 .Returns(new QueryCollection(new Dictionary<string, StringValues> { { "isValid", "ff" } }));
+            var bindingContext = context.Object;
+
+            await binder.BindModelAsync(bindingContext);
+
+            Assert.AreEqual(bindingContext.Result.IsModelSet, false);
+            Assert.IsNull(bindingContext.Result.Model);
+        }
+
+        [TestMethod]
+        public async Task OptionsBinder_NoValueInQueryString()
+        {
+            var dbHelper = new DbHelper();
+            var binder = new OptionsBinder(dbHelper);
+
+            var context = new Mock<ModelBindingContext>();
+            context.Setup(c => c.FieldName).Returns("isValid");
+            context.SetupProperty(c => c.Result);
+
+            context.Setup(c => c.HttpContext.Request.Query)
+                .Returns(new QueryCollection(new Dictionary<string, StringValues>()));
             var bindingContext = context.Object;
 
             await binder.BindModelAsync(bindingContext);
--- ModelBinder/Controllers/WeatherForecastController.cs
+++ ModelBinder/Controllers/WeatherForecastController.cs
@@ -12,20 +12,16 @@ namespace ModelBinder.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-
-
         [HttpGet("")]
         public IEnumerable<string> Get([ModelBinder(typeof(ValueToListBinder<int>))] List<int> ids)
         {
             return ids.Select(i => Summaries[i]);
         }
 
-
-
         [HttpGet("valid/{id}")]
         public string Get(int id, [ModelBinder(typeof(OptionsBinder))] bool isValid)
         {
-            return isValid ? "valid" : "notValid";
+            return $"{id} is {(isValid ? "valid" : "notValid")}";
         }
     }
 }
